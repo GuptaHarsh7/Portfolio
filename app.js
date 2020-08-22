eefc9904fdaf4fd633c70fea0dcf844eb62e0cb1
Harsh Gupta <harshgupta6641@gmail.com>
2020-08-22 13:48:31 +0000

    website creation

---
+++ app.js
@@ -0,0 +1,18 @@
+var express               = require("express"),
+    bodyParser            = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+app.listen(3000,process.env.IP,function(){
+    console.log("Connected");
+});
